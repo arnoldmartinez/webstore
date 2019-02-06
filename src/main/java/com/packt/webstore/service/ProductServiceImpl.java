dd460f36f117a5f019850291d2b7053bc3bdf930
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2019-02-05 20:22:31 -0600

    Create a product service

---
+++ src/main/java/com/packt/webstore/service/ProductServiceImpl.java
@@ -0,0 +1,20 @@
+package com.packt.webstore.service;
+
+import com.packt.webstore.domain.Product;
+import com.packt.webstore.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.getAllProducts();
+    }
+}
