8f468f2bb15d7c2b20dc8ac4298fd7c96e16261d
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2019-03-05 06:30:36 -0600

    Configuring ContentNegotiatingViewResolver

--- src/main/java/com/packt/webstore/domain/Product.java
+++ src/main/java/com/packt/webstore/domain/Product.java
@@ -1,11 +1,15 @@
 package com.packt.webstore.domain;
 
 import lombok.Data;
+import org.codehaus.jackson.annotate.JsonIgnore;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.math.BigDecimal;
 
 @Data
+@XmlRootElement
 public class Product {
 
     private String productId;
@@ -18,7 +22,11 @@ public class Product {
     private long unitInOrder;
     private boolean discontinued;
     private String condition;
+
+    @JsonIgnore
     private MultipartFile productImage;
+
+    @JsonIgnore
     private MultipartFile productPDF;
 
     public Product() {
@@ -31,4 +39,13 @@ public class Product {
         this.unitPrice = unitPrice;
     }
 
+    @XmlTransient
+    public MultipartFile getProductImage() {
+        return productImage;
+    }
+
+    @XmlTransient
+    public MultipartFile getProductPDF() {
+        return productPDF;
+    }
 }
