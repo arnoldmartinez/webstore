b27aa025da9c0f6fd6081f78f1c7c0cfa1cd034d
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2019-03-26 21:20:52 -0600

    Unit testing domain objects

--- src/main/java/com/packt/webstore/domain/CartItem.java
+++ src/main/java/com/packt/webstore/domain/CartItem.java
@@ -21,7 +21,7 @@ public class CartItem implements Serializable {
     private BigDecimal totalPrice;
 
     public CartItem() {
-        // TODO Auto-generated constructor stub
+        this.quantity = 1;
     }
 
     public CartItem(Product product) {
